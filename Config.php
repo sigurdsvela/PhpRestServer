07147bd321c2a6fccf19706227510d24e9f2f89a
Sigurd Berg Svela <sigurdbergsvela@gmail.com>
2014-01-22 21:31:20 +0100

    Implement Config class

--- Config.php
+++ Config.php
@@ -4,17 +4,58 @@ namespace rest;
 use std\json\JSON;
 
 class Config {
+	/**
+	 * @var \std\json\JSON
+	 */
 	private $config;
+
+	/**
+	 * @var
+	 */
+	private $version;
 	
 	/**
 	 * @param JSON $config
 	 */
 	public function __construct(JSON $config) {
 		$this->config = $config;
+		$this->initValue("version", "1.0");
+		$this->initValue("base", "/");
+		$this->initValue("host", null);
+		$this->initValue("controllers", array());
+		$this->initValue("urlMapping", array());
 	}
-	
+
+	/**
+	 * Set the config valut of $key to $value
+	 * if it is not already set.
+	 *
+	 * @param $key
+	 * @param $value
+	 *
+	 * @return void
+	 */
+	private function initValue($key, $value) {
+		$this->config[$key] = isset($this->config[$key]) ? $this->config[$key] : $value; 
+	}
+
+	/**
+	 * Get the base path that this server should respond to
+	 *
+	 * @return string
+	 */
 	public function getBase() {
-		
+		return $this->config["base"];
+	}
+
+	/**
+	 * Get the host name that this server should
+	 * respond to, or null if any
+	 *
+	 * @return string|null
+	 */
+	public function getHost() {
+		return $this->config["host"];
 	}
 	
 	public function getControllers() {
@@ -26,14 +67,25 @@ class Config {
 	}
 
 	/**
-	 * Get a controller for a specified URL
+	 * Get a controller for a specified URL, or null if none is spesified.
 	 *
 	 * @param $url
 	 *
 	 * @return Controller
 	 */
 	public function getController($url) {
-		
+		foreach($this->getUrlMapping() as $map => $controller) {
+			$matches = array();
+			if (preg_match("/^" . $map . "/", $url, $matches)) {
+				$controller = $this->getControllers()[$controller];
+				/**
+				 * @var $c Controller
+				 */
+				$c = new $controller();
+				$c->
+			}
+		}
+		return null;
 	}
 	
 } 
